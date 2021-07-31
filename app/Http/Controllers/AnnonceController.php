e6404f1341ae497a5b41142a6cb718b3aab9f741
yassine_desktop <elmeftouhi@extensionmedia.ma>
2021-07-31 22:00:47 +0100

    annonce images

--- app/Http/Controllers/AnnonceController.php
+++ app/Http/Controllers/AnnonceController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Annonce;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class AnnonceController extends Controller
 {
-    //
+    public function create_slug(){
+        $annonces = Annonce::all();
+        foreach($annonces as $a){
+            $a->slug = Str::slug($a->titre)."-".$a->id;
+            echo $a->save();
+        }
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,17 @@
 <?php
 
+use App\Http\Controllers\AnnonceController;
 use App\Http\Controllers\StartController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [StartController::class, 'index'])->name('start');
-Route::get('/slug', [StartController::class, 'slug']);
+Route::get('/annonce_slug', [AnnonceController::class, 'create_slug']);
 
 
 Route::get('/search/annonce_category/{id}', [StartController::class, 'getSubAnnonceCategorie']);
 Route::get('/search/city_sector/{id}', [StartController::class, 'getCitySectors']);
+
 Route::get('/annonce/{slug}-{uid}', function(){
     return view('annonce.show.index');
 });
