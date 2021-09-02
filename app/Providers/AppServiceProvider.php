c04fd1269894a85a70bafd30cf974fb6f9ad7b76
yassine_desktop <elmeftouhi@extensionmedia.ma>
2021-09-03 00:48:53 +0100

    remove https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,9 +26,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::defaultView('vendor.pagination.tailwind');
-
-        if (env('APP_ENV') === 'production') {
-            \Illuminate\Support\Facades\URL::forceScheme('https');
-        }
     }
 }
